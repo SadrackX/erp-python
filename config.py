f4e447959c0d69ab0bee6404e6e0e8083c37fa16
SadrackX <thallyelsom@hotmail.com>
2025-07-12 16:12:58 -0300

    rebase

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+CLIENTES_CSV = os.path.join(DATA_DIR, 'clientes.csv')
+PRODUTOS_CSV = os.path.join(DATA_DIR, 'produtos.csv')
+VENDAS_CSV = os.path.join(DATA_DIR, 'vendas.csv')
